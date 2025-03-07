3dd76c6fc33055862509216b53f70cb949a1352e
coachonye <onye.iwuorie@gmail.com>
2025-03-06 18:36:25 -0800

    auth template

--- eslint.config.js
+++ eslint.config.js
@@ -22,16 +22,12 @@ export default [
     },
     rules: {
       ...js.configs.recommended.rules,
-      ...react.configs.recommended.rules,
-      ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
-      'react/jsx-no-target-blank': 'off',
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
-      'react/prop-types': 'off', // add this line
-      'react/no-unescaped-entities': 'off', // add this line
     },
   },
 ]
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,13 +1,15 @@
 // src/components/NavBar/NavBar.jsx
 
+import { Link } from 'react-router';
+
 const NavBar = () => {
-    return (
-      <nav>
-        <p>Navbar contents will go here.</p>
-      </nav>
-    );
-  };
-  
-  export default NavBar;
-  
-  
+  return (
+    <nav>
+      <ul>
+        <li><Link to='/sign-up'>Sign Up</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
